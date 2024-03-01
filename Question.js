8542ec0a58567d896304a0508e92ae79fdfedcb8
LebedevD174 <Lebedevdima174@yandex.ru>
2024-03-01 13:52:52 +0300

    create Question

---
+++ Question.js
@@ -0,0 +1,39 @@
+class Question {
+  #answers;
+
+  #question;
+
+  constructor({ question, answers }) {
+    this.#question = question;
+    this.#answers = answers;
+  }
+
+  #transform() {
+    const answers = this.#answers.replace('\r\n', ' ').trim().split('\r\n');
+    return answers;
+  }
+
+  get answers() {
+    return this.#transform().map((answer, index) => {
+      if (answer[0] === '$') {
+        return { name: `${index + 1}) ${answer.slice(1)}`, value: 10 };
+      }
+      return { name: `${index + 1}) ${answer}`, value: 0 };
+    });
+  }
+
+  get question() {
+    return this.#question;
+  }
+}
+module.exports = Question;
+
+const cont = new Question({ question: 'Как спать', answers: 'Лежа\nСтоя\n\$За кодом' });
+const cooo = new Question({
+  question: '\r\n Как Андрею сделать лекции еще продуктивнее?\r\n',
+  answers: '\r\n' + 'Проводить их вместе с Даней\r\n'
+  + '$Купить новую клавиатуру\r\n' + 'Писать код на Python\r\n'
+});
+
+console.log(cooo.question);
+console.log(cooo.answers);
